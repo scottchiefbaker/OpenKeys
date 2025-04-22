2db9268e9a294fb4b62767fe16138774571af9a1
feive7 <feive7@gmail.com>
2025-04-22 07:23:44 -0700

    added a little icon of me to show jankily in the about menu

--- OpenKeys/resource.h
+++ OpenKeys/resource.h
@@ -12,6 +12,7 @@
 #define IDI_SMALL                       108
 #define IDC_OPENKEYS                    109
 #define IDR_MAINFRAME                   128
+#define IDI_ICON1                       130
 #define ID_Menu                         32771
 #define IDM_REFRESH_BUTTON              32772
 #define IDC_STATIC                      -1
@@ -21,7 +22,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32774
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
