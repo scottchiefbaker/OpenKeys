163b1a7d4a6f403dd4e7947a352bc1ac09a0f028
Scott Baker <scott.baker@cbsoregon.com>
2025-06-23 10:59:42 -0700

    Add a little error handling to writing the new JSON file

--- OpenKeys/OpenKeys.cpp
+++ OpenKeys/OpenKeys.cpp
@@ -408,10 +408,13 @@ void LoadShortcuts() {
                 }
 
                 // Write the new JSON to the local file
-				std::ofstream file;
-				file.open("shortcuts.json");
-				file << jsonURL.dump(4); // Pretty print with 4 spaces
-				file.close();
+                std::ofstream file("shortcuts.json");
+                if (file.is_open()) {
+                    file << jsonURL.dump(4); // Pretty print with 4 spaces
+                } else {
+                    log_line("Failed to open shortcuts.json for writing.");
+                }
+                file.close();
 
 				log_line("Overwrote local JSON with the one from the URL");
 
